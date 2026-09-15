6f0d2569365e891992aafa510f25fffdc2631a61
garrettmend <u1305528@umail.utah.edu>
2026-09-15 10:06:05 -0600

    seed

--- InventoryService/obj/Debug/net10.0/InventoryService.AssemblyInfo.cs
+++ InventoryService/obj/Debug/net10.0/InventoryService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a5d17794faa21b471fbe75d47568beb90911ead9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ea9fe1bad4e393afd26fcd1e45ebfdeb928e016")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
